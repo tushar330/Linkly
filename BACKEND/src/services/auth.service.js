bd4abe6feab29b78e7070139170a6722223ecb2f
tushar330 <tushartiwari338@gmail.com>
2025-12-18 03:48:33 +0530

    fix: 401 on login error and SPA routing refresh 404

--- BACKEND/src/services/auth.service.js
+++ BACKEND/src/services/auth.service.js
@@ -1,5 +1,5 @@
 import { createUser, findUserByEmail, findUserByEmailByPassword } from "../dao/user.dao.js"
-import { ConflictError } from "../utils/errorHandler.js"
+import { ConflictError, UnauthorizedError } from "../utils/errorHandler.js"
 import {signToken} from "../utils/helper.js"
 
 export const registerUser = async (name, email, password) => {
@@ -12,10 +12,10 @@ export const registerUser = async (name, email, password) => {
 
 export const loginUser = async (email, password) => {
     const user = await findUserByEmailByPassword(email)
-    if(!user) throw new Error("Invalid email or password")
+    if(!user) throw new UnauthorizedError("Invalid email or password")
 
     const isPasswordValid = await user.comparePassword(password)
-    if(!isPasswordValid) throw new Error("Invalid email or password")
+    if(!isPasswordValid) throw new UnauthorizedError("Invalid email or password")
     const token = signToken({id: user._id})
     return {token,user}
 }
