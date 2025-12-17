e101a55b35a1598346875ac909804dc9670362c7
tushar330 <tushartiwari338@gmail.com>
2025-12-18 03:26:19 +0530

    fix: Use process.env.PORT instead of hardcoded 3000

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -36,9 +36,10 @@ app.get("/:id",redirectFromShortUrl)
 
 app.use(errorHandler)
 
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
     connectDB()
-    console.log("Server is running on http://localhost:3000");
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
 
 // GET - Redirection 
